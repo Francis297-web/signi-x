d2eebabe5f34e32b9951d249980430f234e39857
Francis297-web <francischege381@gmail.com>
2026-07-16 02:05:03 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,5 @@
 // Supabase Configuration
-
 const SUPABASE_URL = "https://ocfdsuthkdtcpjliuesz.supabase.co";
-
 const SUPABASE_ANON_KEY = "sb_publishable_LWDSOTkh_Nvjtk3SeN7r2A_244qkVLP";
 
 const supabaseClient = window.supabase.createClient(
@@ -9,4 +7,7 @@ const supabaseClient = window.supabase.createClient(
     SUPABASE_ANON_KEY
 );
 
-console.log(" Supabase connected successfully!");
+// make it accessible from index.html render() etc
+window.supabaseClient = supabaseClient;
+
+console.log("Supabase connected successfully!", supabaseClient);
